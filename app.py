4bc6797ff3bf98d3367599c931d557ab32e0da9b
lfborjas <luis.borjas@escolarea.com>
2011-12-03 00:26:22 -0600

    agregadas citas

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from cgi import parse_qs
 from wsgiref.simple_server import make_server
+
 QUOTES = [
-    {'author': '' , 'content': ''},
-    {'author': '' , 'content': ''},
-    {'author': '' , 'content': ''},
-    {'author': '' , 'content': ''},
-    {'author': '' , 'content': ''},
-    {'author': '' , 'content': ''}
+    {'author': 'Ken Thompson' , 'content': 'When in doubt, use brute force'},
+    {'author': 'Brian W. Kernighan' , 'content': 'The most effective debugging tool is still careful thought, coupled with judiciously placed print statements'},
+    {'author': 'E.W. Dijkstra' , 'content': 'Simplicity is prerequisite for reliability'},
+    {'author': 'E.W. Dijkstra' , 'content': "The computing scientist's main challenge is not to get confused by the complexities of his own making"},
+    {'author': 'Ken Thompson' , 'content': 'One of my most productive days was throwing away 1000 lines of code'},
+    {'author': 'Jeff Sickel' , 'content': 'Deleted code is debugged code'}
 ]
 
 def application(environ, response_callback):
